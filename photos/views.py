c178ab9bc1e116149cd9741e28b4305abed29445
Josephshitandi <jsphshtnd@gmail.com>
2020-11-13 20:53:28 +0300

    Create my_gallery method in views.py

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
+def my_gallery(request):
+    date = dt.date.today()
+    return render(request, 'gallery.html', {"date": date})
