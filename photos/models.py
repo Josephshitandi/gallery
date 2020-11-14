df619f04d609275a2ee50c4a48c5811a01c78517
Josephshitandi <jsphshtnd@gmail.com>
2020-11-14 10:52:22 +0300

    Add class Location and Category

--- photos/models.py
+++ photos/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+import cloudinary
 from cloudinary.models import CloudinaryField
 import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=60)
 class Image(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=60)
     description = models.TextField()
     author = models.CharField(max_length=40, default='admin')
     date = models.DateTimeField(auto_now_add=True)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    
+  
+
+    
+    
