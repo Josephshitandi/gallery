63c08c7d1c12b770b835b95b91a874b1f565c5e8
Josephshitandi <jsphshtnd@gmail.com>
2020-11-15 18:05:51 +0300

    Add filter by location function

--- photos/urls.py
+++ photos/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns =[
     path('', views.my_gallery, name = 'myGallery'),
     path('search/', views.search_results, name='search_results'),
+    re_path('search/location', views.search_location, name='search_location'),
 ]
