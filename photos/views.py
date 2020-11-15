04b8e95e2f5e4acef0729a89ec8af442216e0b1e
Josephshitandi <jsphshtnd@gmail.com>
2020-11-15 08:40:30 +0300

    Modifies my_gallery method to display images

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Image,Location,Category
 
 # Create your views here.
 def my_gallery(request):
     date = dt.date.today()
-    return render(request, 'gallery.html', {"date": date})
+    images = Image.objects.all()
+    return render(request, 'gallery.html', {"date": date,"images":images})
